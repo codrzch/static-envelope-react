e499cbe3b848e72b793eb8a9cc6e80fc4b7f2791
Zach Mathson <32144404+mathsondevelop@users.noreply.github.com>
2018-03-21 21:16:17 -0500

    Create github-lang-fix.cs

---
+++ github-lang-fix.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ForceJavaScriptGitHub
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is a JavaScript repository");
+        }
+    }
+}
